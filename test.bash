2724bada9d329ae23b707aee339f65e4999b5226
Kshitij1K <kabeer@iitk.ac.in>
2020-07-14 23:25:30 +0530

    Add functionality for testing deployment repo commits

--- test.bash
+++ test.bash
@@ -3,15 +3,19 @@
 DOCKERFILECHECK=$(git diff --cached --name-status | grep 'Dockerfile')
 
 if [ -z "$DOCKERFILECHECK" ] ; then
-    echo "Dockerfile isn't being committed, so no need to check the commit message"
+    echo "Dockerfile isn't being committed, so no need to check the commit message."
     exit 0
 fi
 
 DOCKERREPO=$(git branch | grep \* | sed 's/* deploy\///' | sed 's/* test\///')
 
+DEPLOYCHECK=$(git branch | grep \* | sed 's/* deploy\///' | sed 's/'"$DOCKERREPO"'//')
+if [ -z "$DEPLOYCHECK" ] ; then
+    echo "This is a deployment image"
+fi
+
 VERSION=$(grep -oE '\bv([1-9][0-9]*|0)\.([1-9][0-9]*|0)\.([1-9][0-9]*|0)\b' $1 |
         sed 's/v//')
-
 
 if [ -z "$VERSION" ] ; then
     echo "A valid version number was not found in the commit message. Aborting."
@@ -36,7 +40,13 @@ if [ -z "$JQCHECK" ] ; then
     fi
 fi
 
-TAGS=$(curl 'https://registry.hub.docker.com/v2/repositories/ariitk/'"$DOCKERREPO"'/tags/' | jq '."results"[]["name"]' | sed 's/"//g')
+echo $DEPLOYCHECK
+
+if [ -z "$DEPLOYCHECK" ] ; then
+    TAGS=$(curl 'https://registry.hub.docker.com/v2/repositories/ariitk/deployment/tags/' | jq '."results"[]["name"]' | sed 's/"//g' | sed 's/'"$DOCKERREPO"'-//')
+else
+    TAGS=$(curl 'https://registry.hub.docker.com/v2/repositories/ariitk/'"$DOCKERREPO"'/tags/' | jq '."results"[]["name"]' | sed 's/"//g')
+fi
 
 /usr/bin/python3 .git/hooks/check_version.py $VERSION $TAGS
 
@@ -59,13 +69,52 @@ fi
 
 echo "Checking whether image is already built or not."
 
-LOCALCHECK=$(sudo docker images | grep "$DOCKERREPO" | grep "$VERSION")
+if [ -z "$DEPLOYCHECK" ] ; then
+    LOCALCHECK=$(sudo docker images | grep deployment |grep "$DOCKERREPO-$VERSION")
+else
+    LOCALCHECK=$(sudo docker images | grep "$DOCKERREPO" | grep "$VERSION")
+fi 
 
 if [ -n "$LOCALCHECK" ] ; then
     echo "Image already exists on computer."
-
     read -r -p "Do you want to overwrite it? [y/N] " response < /dev/tty
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]] ; then
+        if [ -z "$DEPLOYCHECK" ] ; then
+            echo "Building image with name ariitk/deployment:$DOCKERREPO-$VERSION"
+            if sudo docker build -t ariitk/deployment:$DOCKERREPO-$VERSION . ; then
+                echo "The build succeeded."
+            else
+                echo "The build failed. Please rectify the Dockerfile and try again."
+                echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
+                exit 4
+            fi
+        else
+            echo "Building image with name ariitk/$DOCKERREPO:$VERSION"
+            if sudo docker build -t ariitk/$DOCKERREPO:$VERSION . ; then
+                echo "The build succeeded."
+            else
+                echo "The build failed. Please rectify the Dockerfile and try again."
+                echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
+                exit 4
+            fi
+        fi        
+
+    else
+        echo "Aborting. Please give a unique name and try again."
+        echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
+        exit 5
+    fi
+else
+    if [ -z "$DEPLOYCHECK" ] ; then
+        echo "Building image with name ariitk/deployment:$DOCKERREPO-$VERSION"
+        if sudo docker build -t ariitk/deployment:$DOCKERREPO-$VERSION . ; then
+            echo "The build succeeded."
+        else
+            echo "The build failed. Please rectify the Dockerfile and try again."
+            echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
+            exit 4
+        fi
+    else
         echo "Building image with name ariitk/$DOCKERREPO:$VERSION"
         if sudo docker build -t ariitk/$DOCKERREPO:$VERSION . ; then
             echo "The build succeeded."
@@ -74,26 +123,47 @@ if [ -n "$LOCALCHECK" ] ; then
             echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
             exit 4
         fi
-    else
-        echo "Aborting. Please give a unique name and try again."
-        echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
+    fi
+fi
+
+
+echo "Pushing the image(s) to Dockerhub. Please enter your credentials if required."
+
+if [ -z "$DEPLOYCHECK" ] ; then
+    if sudo docker login && sudo docker push ariitk/deployment:$DOCKERREPO-$VERSION ; then
+        echo "Push succeeded."
+    else 
+        echo "Push to Dockerhub failed. Maybe check your internet connection/enter correct credentials"
         exit 5
     fi
+
+    if [ $VERSIONCHECK -eq 0 ] ; then
+        echo "Since this is the latest version, also building and pushing image with name ariitk/deployment:$DOCKERREPO-latest"
+        sudo docker build -t ariitk/deployment:$DOCKERREPO-latest .
+        if sudo docker login && sudo docker push ariitk/deployment:$DOCKERREPO-latest ; then
+            echo "Push succeeded."
+        else 
+            echo "Push to Dockerhub failed. Maybe check your internet connection/enter correct credentials"
+            exit 5
+        fi
+    fi
 else
-    echo "Building image with name ariitk/$DOCKERREPO:$VERSION"
-    if sudo docker build -t ariitk/$DOCKERREPO:$VERSION . ; then
-        echo "The build succeeded."
-    else
-        echo "The build failed. Please rectify the Dockerfile and try again."
-        echo "Or, run git commit with the '--no-verify' option to skip checking and building the Dockerfile, and pushing the image to DockerHub (Not recommended)"
-        exit 4
+    if sudo docker login && sudo docker push ariitk/$DOCKERREPO:$VERSION ; then
+        echo "Push succeeded."
+    else 
+        echo "Push to Dockerhub failed. Maybe check your internet connection/enter correct credentials"
+        exit 5
+    fi
+
+    if [ $VERSIONCHECK -eq 0 ] ; then
+        echo "Since this is the latest version, also building and pushing image with name ariitk/$DOCKERREPO:latest"
+        sudo docker build -t ariitk/$DOCKERREPO:latest .
+        if sudo docker login && sudo docker push ariitk/$DOCKERREPO:latest ; then
+            echo "Push succeeded."
+        else 
+            echo "Push to Dockerhub failed. Maybe check your internet connection/enter correct credentials"
+            exit 5
+        fi
     fi
 fi
-
-echo "Pushing image to Dockerhub. Please enter your credentials if required."
-
-if sudo docker login && sudo docker push ariitk/$DOCKERREPO:$VERSION ; then
-    echo "Push succeeded. Committing the Dockerfile."
-else 
-    echo "Push to Dockerhub failed. Maybe check your internet connection/entire correct credentials"
-fi
+echo "Commiting the Dockerfile."
