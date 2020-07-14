ab33d6457334148ba3376e6434f3360fccaa05a7
Kshitij1K <kabeer@iitk.ac.in>
2020-07-14 19:55:16 +0530

    Physcially copying files

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 CURRDIR=$(dirname "$0")
 
-ln $CURRDIR/test.bash $CURRDIR/.git/hooks/commit-msg
-ln $CURRDIR/check_version.py $CURRDIR/.git/hooks/check_version.py
+cp $CURRDIR/test.bash $CURRDIR/.git/hooks/commit-msg
+cp $CURRDIR/check_version.py $CURRDIR/.git/hooks/check_version.py
 
 sudo chmod +x $CURRDIR/.git/hooks/commit-msg $CURRDIR/.git/hooks/check_version.py
