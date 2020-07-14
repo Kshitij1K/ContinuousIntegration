7cc6a7ed2ea14a4e519b6c3795841a3c20083e19
Kshitij1K <kabeer@iitk.ac.in>
2020-07-14 19:50:23 +0530

    Change symlinkind from soft to hard, give execute permission.

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
-CURRDIR=$(dirname "$0")
+ln $CURRDIR/test.bash $CURRDIR/.git/hooks/commit-msg
+ln $CURRDIR/check_version.py $CURRDIR/.git/hooks/check_version.py
 
-ln -s $CURRDIR/test.bash $CURRDIR/.git/hooks/commit-msg
-ln -s $CURRDIR/check_version.py $CURRDIR/.git/hooks/check_version.py
+sudo chmod +x $CURRDIR/.git/hooks/commit-msg $CURRDIR/.git/hooks/check_version.py
